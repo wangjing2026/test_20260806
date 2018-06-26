36b3b0ea98f26df0cb7ea2cc088a818fd30389c8
T4659925 <kazuki.miyamoto1@jp.ricoh.com>
2018-06-26 21:21:57 +0900

    Changeset:792816996 Requests:[]
    Updated for Top Access (Guest Logon TO SMB)(FCH_MS017457)

--- 05_Implement/SYSROM_SRC/single_build.sh
+++ 05_Implement/SYSROM_SRC/single_build.sh
@@ -527,7 +527,7 @@ $ROOT_DIR/dev/AL/Network/NMO/smb
 #$ROOT_DIR/dev/AL/ApplicationServers/ContentWebServer
 #$ROOT_DIR/dev/AL/ApplicationServers/FileDownloader
 #$ROOT_DIR/dev/AL/ApplicationServers/eFIRemoteIFServer
-#$ROOT_DIR/dev/AL/TopAccess
+$ROOT_DIR/dev/AL/TopAccess
 #$ROOT_DIR/dev/AL/Stage2
 #$ROOT_DIR/dev/AL/AppAccessor
 #$ROOT_DIR/dev/AL/GSI
